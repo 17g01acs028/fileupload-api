07df7a5bf1844dcc788ae64f9f95bfbe230ad3a9
Stephen Mutio <stevemutioo123@gmail.com>
2023-11-11 13:46:26 +0300

    Corected env error

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const s3 = new AWS.S3()
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: env.process.BUCKET,
+    bucket: process.env.BUCKET,
     key: function (req, file, cb) {
       cb(null, 'uploads/' + file.originalname);
     },
